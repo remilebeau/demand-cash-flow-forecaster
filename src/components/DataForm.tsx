a613e5f329efab5bbd332f2028f4df3dbccce145
remilebeau <remilebeau90@gmail.com>
2024-06-24 14:52:09 -0400

    added Zod form

--- src/components/DataForm.tsx
+++ src/components/DataForm.tsx
@@ -1,13 +1,23 @@
 "use client";
 import { useState } from "react";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 export default function DataForm() {
-  // form schema
+  // define form schema
 
   const formSchema = z.object({
     distMin: z.number({
@@ -35,118 +45,97 @@ export default function DataForm() {
     simDaysPerYear: z.number({ message: "Days per year is required" }),
   });
 
-  const router = useRouter();
+  // define form
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      distMin: undefined,
+      distMode: undefined,
+      distMax: undefined,
+      simDaysPerYear: undefined,
+    },
+  });
 
-  const [distMin, setDistMin] = useState<number>();
-  const [distMode, setDistMode] = useState<number>();
-  const [distMax, setDistMax] = useState<number>();
-  const [simDaysPerYear, setSimDaysPerYear] = useState<number>();
-  const [errMsg, setErrMsg] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setIsLoading(true);
-    setErrMsg("");
-    // check for undefined
-    if (
-      distMin === undefined ||
-      distMode === undefined ||
-      distMax === undefined ||
-      simDaysPerYear === undefined
-    ) {
-      setErrMsg("All fields are required");
-      setIsLoading(false);
-      return;
-    }
-    // check for min <= mode <= max
-    if (distMode < distMin || distMode > distMax) {
-      setErrMsg("Expected value must be between minimum and maximum");
-      setIsLoading(false);
-      return;
-    }
-    // check for min < max
-    if (distMin >= distMax) {
-      setErrMsg("Minimum must be less than maximum");
-      setIsLoading(false);
-      return;
-    }
-
+  // define submit handler
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const { distMin, distMode, distMax, simDaysPerYear } = values;
     router.push(
       `/results?distMin=${distMin}&distMode=${distMode}&distMax=${distMax}&simDaysPerYear=${simDaysPerYear}`,
     );
-    setIsLoading(false);
-  };
+  }
+
+  const router = useRouter();
 
   return (
-    <article className="flex flex-col gap-4 rounded-md border border-border p-4">
-      <h1 className="text-3xl font-bold">Cash Flow Simulator</h1>
-      <h2 className="text-2xl">
-        Enter a daily minimum, mode, maximum, and days per month. These values
-        will be used to create a triangular distribution. And from that
-        distribution, 1000 simulations of yearly cash flow.
-      </h2>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-        <Label htmlFor="distMin" className="text-xl">
-          Minimum Value:
-        </Label>
-        <Input
-          className="text-xl"
-          required
-          type="number"
-          value={distMin}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setDistMin(parseInt(e.target.value))
-          }
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="distMin"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Min</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter Minimum" {...field} />
+              </FormControl>
+              <FormDescription>
+                The minimum cash flow per period.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-        <Label htmlFor="distMode" className="text-xl">
-          Expected Value:
-        </Label>
-        <Input
-          className="text-xl"
-          required
-          type="number"
-          value={distMode}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setDistMode(parseInt(e.target.value))
-          }
-        />
-        <Label htmlFor="distMax" className="text-xl">
-          Maximum Value:
-        </Label>
-        <Input
-          className="text-xl"
-          required
-          type="number"
-          value={distMax}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setDistMax(parseInt(e.target.value))
-          }
-        />
-        <Label htmlFor="simDaysPerYear" className="text-xl">
-          Days Per Month:
-        </Label>
-        <Input
-          className="text-xl"
-          required
-          type="number"
-          value={simDaysPerYear}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setSimDaysPerYear(parseInt(e.target.value))
-          }
-        />
-        <Button type="submit" className="text-xl">
-          Submit
-        </Button>
-      </form>
 
-      {errMsg && <p className="text-red-500">{errMsg}</p>}
-      {isLoading && (
-        <>
-          <p>Loading...</p>
-          <p>The first request may take up to 60 seconds.</p>
-        </>
-      )}
-    </article>
+        <FormField
+          control={form.control}
+          name="distMode"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Mode</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter Mode" {...field} />
+              </FormControl>
+              <FormDescription>
+                The most likely cash flow per period.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="distMax"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Max</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter Maximum" {...field} />
+              </FormControl>
+              <FormDescription>
+                The highest cash flow per period.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="simDaysPerYear"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Periods per Year</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter Periods per Year" {...field} />
+              </FormControl>
+              <FormDescription>The number of periods per year.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }
