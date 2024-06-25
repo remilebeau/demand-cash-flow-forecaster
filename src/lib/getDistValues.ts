8442c6a778504a4caecdc885c67fff4c6416cbf8
remilebeau <remilebeau90@gmail.com>
2024-06-25 14:37:33 -0400

    Fixed DATA_URL in production environment

--- src/lib/getDistValues.ts
+++ src/lib/getDistValues.ts
@@ -5,7 +5,7 @@ export async function getDistValues(
 ) {
   const DATA_URL =
     process.env.NODE_ENV === "production"
-      ? "https://simulation-api-e49j.onrender.com/api/distributions/"
+      ? `https://simulation-api-e49j.onrender.com/api/distributions/?distMin=${distMin}&distMode=${distMode}&distMax=${distMax}`
       : `http://localhost:8000/api/distributions/?distMin=${distMin}&distMode=${distMode}&distMax=${distMax}`;
   const res = await fetch(DATA_URL, {
     method: "GET",
