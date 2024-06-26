4f177e3b24640af478ce09346ff9557a09a64c82
remilebeau <remilebeau90@gmail.com>
2024-06-26 19:26:49 -0400

    revise instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function HomePage() {
     <main className="mx-auto flex max-w-4xl flex-col gap-8 p-8">
       <h1 className="text-3xl font-bold">Demand / Cash Flow Simulator</h1>
       <p>
-        Per period (e.g. daily, weekly, monthly, quarterly), enter a minimum,
-        expected, and maximum demand / cash flow. Using a Monte Carlo simulation
-        of a triangular distribution, these values will be used to forecast an
+        Enter a minimum, expected, and maximum demand / cash flow per period
+        (e.g. daily, weekly, monthly, quarterly). Using a Monte Carlo simulation
+        of a triangular distribution, these values will be used to forecast
         annual demand / cash flow.
       </p>
       <DataForm />
