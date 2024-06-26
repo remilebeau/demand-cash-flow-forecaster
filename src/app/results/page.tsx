11e4f12c13a21d92ec692287bc336a7be0d49519
remilebeau <remilebeau90@gmail.com>
2024-06-26 12:15:45 -0400

    continue styling results

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -31,6 +31,7 @@ export default async function ResultsPage() {
       {distValues && simValues && (
         <main className="mx-auto flex max-w-4xl flex-col gap-8 p-8">
           <Button onClick={() => router.push("/")}>Go Back</Button>
+          <h1 className="text-center text-3xl font-bold">Results</h1>
           <DistPlot distValues={distValues} />
           <SimPlotWithStats simValues={simValues} />
         </main>
