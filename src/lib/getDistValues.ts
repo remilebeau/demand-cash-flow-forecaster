bdf60cd734c1edd5b20061d97ec0fc24139ffab2
remilebeau <remilebeau90@gmail.com>
2024-06-28 20:21:39 -0400

    updated lib functions for new api

--- src/lib/getDistValues.ts
+++ src/lib/getDistValues.ts
@@ -5,7 +5,7 @@ export async function getDistValues(
 ) {
   const DATA_URL =
     process.env.NODE_ENV === "production"
-      ? `https://simulation-api-t28w.onrender.com/api/distributions/?distMin=${distMin}&distMode=${distMode}&distMax=${distMax}`
+      ? `https://simulation-api-t28w.onrender.com/api/distributions/triangular?distMin=${distMin}&distMode=${distMode}&distMax=${distMax}`
       : `http://localhost:8000/api/distributions/?distMin=${distMin}&distMode=${distMode}&distMax=${distMax}`;
   const res = await fetch(DATA_URL, {
     method: "GET",
